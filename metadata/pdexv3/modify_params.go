56e3c67be5d0c6c954979d5a7541095345d7160d
thanhn-inc <thanh.n@incognito.org>
2021-12-22 07:04:41 +0000

    Update modifyParam

--- metadata/pdexv3/modify_params.go
+++ metadata/pdexv3/modify_params.go
@@ -2,28 +2,42 @@ package pdexv3
 
 import (
 	"encoding/json"
-	"github.com/incognitochain/go-incognito-sdk-v2/rpchandler/jsonresult"
-
 	"github.com/incognitochain/go-incognito-sdk-v2/common"
 	metadataCommon "github.com/incognitochain/go-incognito-sdk-v2/metadata/common"
 )
 
+type Pdexv3Params struct {
+	DefaultFeeRateBPS               uint            // the default value if fee rate is not specific in FeeRateBPS (default 0.3% ~ 30 BPS)
+	FeeRateBPS                      map[string]uint // map: pool ID -> fee rate (0.1% ~ 10 BPS)
+	PRVDiscountPercent              uint            // percent of fee that will be discounted if using PRV as the trading token fee (default: 25%)
+	TradingProtocolFeePercent       uint            // percent of fees that is rewarded for the core team (default: 0%)
+	TradingStakingPoolRewardPercent uint            // percent of fees that is distributed for staking pools (PRV, PDEX, ..., default: 10%)
+	PDEXRewardPoolPairsShare        map[string]uint // map: pool pair ID -> PDEX reward share weight
+	StakingPoolsShare               map[string]uint // map: staking tokenID -> pool staking share weight
+	StakingRewardTokens             []common.Hash   // list of staking reward tokens
+	MintNftRequireAmount            uint64          // amount prv for depositing to pdex
+	MaxOrdersPerNft                 uint            // max orders per nft
+	AutoWithdrawOrderLimitAmount    uint            // max orders will be auto withdraw each shard for each blocks
+	MinPRVReserveTradingRate        uint64          // min prv reserve for checking price of trading fee paid by PRV
+	OrderMiningRewardRatioBPS       map[string]uint // map: pool ID -> ratio of LOP rewards compare with LP rewards (0.1% ~ 10 BPS)
+}
+
 type ParamsModifyingRequest struct {
 	metadataCommon.MetadataBaseWithSignature
-	jsonresult.Pdexv3Params `json:"Pdexv3Params"`
+	Pdexv3Params `json:"Pdexv3Params"`
 }
 
 type ParamsModifyingContent struct {
-	Content  jsonresult.Pdexv3Params `json:"Content"`
-	ErrorMsg string                  `json:"ErrorMsg"`
-	TxReqID  common.Hash             `json:"TxReqID"`
-	ShardID  byte                    `json:"ShardID"`
+	Content  Pdexv3Params `json:"Content"`
+	ErrorMsg string       `json:"ErrorMsg"`
+	TxReqID  common.Hash  `json:"TxReqID"`
+	ShardID  byte         `json:"ShardID"`
 }
 
 type ParamsModifyingRequestStatus struct {
-	Status                  int    `json:"Status"`
-	ErrorMsg                string `json:"ErrorMsg"`
-	jsonresult.Pdexv3Params `json:"Pdexv3Params"`
+	Status       int    `json:"Status"`
+	ErrorMsg     string `json:"ErrorMsg"`
+	Pdexv3Params `json:"Pdexv3Params"`
 }
 
 func (paramsModifying ParamsModifyingRequest) Hash() *common.Hash {
