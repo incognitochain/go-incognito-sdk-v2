5047a02762fd96b1261834ff82cdb0f1082f5f91
thanhn-inc <thanh.n@incognito.org>
2021-12-22 04:57:46 +0000

    Update modifyParam

--- metadata/pdexv3/modify_params.go
+++ metadata/pdexv3/modify_params.go
@@ -2,40 +2,28 @@ package pdexv3
 
 import (
 	"encoding/json"
+	"github.com/incognitochain/go-incognito-sdk-v2/rpchandler/jsonresult"
 
 	"github.com/incognitochain/go-incognito-sdk-v2/common"
 	metadataCommon "github.com/incognitochain/go-incognito-sdk-v2/metadata/common"
 )
 
-type Pdexv3Params struct {
-	DefaultFeeRateBPS               uint            `json:"DefaultFeeRateBPS"`
-	FeeRateBPS                      map[string]uint `json:"FeeRateBPS"`
-	PRVDiscountPercent              uint            `json:"PRVDiscountPercent"`
-	TradingProtocolFeePercent       uint            `json:"TradingProtocolFeePercent"`
-	TradingStakingPoolRewardPercent uint            `json:"TradingStakingPoolRewardPercent"`
-	PDEXRewardPoolPairsShare        map[string]uint `json:"PDEXRewardPoolPairsShare"`
-	StakingPoolsShare               map[string]uint `json:"StakingPoolsShare"`
-	StakingRewardTokens             []common.Hash   `json:"StakingRewardTokens"`
-	MintNftRequireAmount            uint64          `json:"MintNftRequireAmount"`
-	MaxOrdersPerNft                 uint            `json:"MaxOrdersPerNft"`
-}
-
 type ParamsModifyingRequest struct {
 	metadataCommon.MetadataBaseWithSignature
-	Pdexv3Params `json:"Pdexv3Params"`
+	jsonresult.Pdexv3Params `json:"Pdexv3Params"`
 }
 
 type ParamsModifyingContent struct {
-	Content  Pdexv3Params `json:"Content"`
-	ErrorMsg string       `json:"ErrorMsg"`
-	TxReqID  common.Hash  `json:"TxReqID"`
-	ShardID  byte         `json:"ShardID"`
+	Content  jsonresult.Pdexv3Params `json:"Content"`
+	ErrorMsg string                  `json:"ErrorMsg"`
+	TxReqID  common.Hash             `json:"TxReqID"`
+	ShardID  byte                    `json:"ShardID"`
 }
 
 type ParamsModifyingRequestStatus struct {
-	Status       int    `json:"Status"`
-	ErrorMsg     string `json:"ErrorMsg"`
-	Pdexv3Params `json:"Pdexv3Params"`
+	Status                  int    `json:"Status"`
+	ErrorMsg                string `json:"ErrorMsg"`
+	jsonresult.Pdexv3Params `json:"Pdexv3Params"`
 }
 
 func (paramsModifying ParamsModifyingRequest) Hash() *common.Hash {
