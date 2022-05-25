67e9cd6998a77bcd58cbf4e748cf60ec7a8c1ece
thanhn-inc <thanh.n@incognito.org>
2022-05-25 11:23:12 +0100

    Add coin-grouping methods for generating OTAs

---
+++ coin/receiver_test.go
@@ -0,0 +1,69 @@
+package coin
+
+import (
+	"fmt"
+	"github.com/incognitochain/go-incognito-sdk-v2/common"
+	"github.com/incognitochain/go-incognito-sdk-v2/key"
+	"github.com/incognitochain/go-incognito-sdk-v2/wallet"
+	"testing"
+	"time"
+)
+
+func TestOTAReceiver_FromCoinParams(t *testing.T) {
+	for i := 0; i < numTests; i++ {
+		prefix := fmt.Sprintf("[TEST %v]", i)
+		common.MaxShardNumber = 1 + common.RandInt()%7
+		senderShard := common.RandInt() % common.MaxShardNumber
+		receiverShard := common.RandInt() % common.MaxShardNumber
+		coinType := common.RandInt() % 2
+		if coinType == PrivacyTypeMint {
+			senderShard = receiverShard
+		}
+		fmt.Printf("%v STARTED\n", prefix)
+		fmt.Printf("%v numShards: %v, senderShard: %v, receiverShard: %v, coinType: %v\n", prefix,
+			common.MaxShardNumber, senderShard, receiverShard, coinType)
+
+		w, err := wallet.GenRandomWalletForShardID(byte(receiverShard))
+		if err != nil {
+			panic(fmt.Sprintf("%v %v", prefix, err))
+		}
+		paymentInfo := &key.PaymentInfo{
+			PaymentAddress: w.KeySet.PaymentAddress,
+			Amount:         0,
+			Message:        []byte{},
+		}
+
+		var coinParam *CoinParams
+		if coinType == PrivacyTypeTransfer {
+			coinParam = NewTransferCoinParams(paymentInfo, byte(senderShard))
+		} else {
+			coinParam = NewMintCoinParams(paymentInfo)
+		}
+
+		start := time.Now()
+		tmp := new(OTAReceiver)
+		err = tmp.FromCoinParams(coinParam)
+		if err != nil {
+			panic(fmt.Sprintf("%v %v", prefix, err))
+		}
+
+		tmpSenderShard, tmpReceiverShard, tmpCoinType, err := tmp.DeriveShardID()
+		if err != nil {
+			panic(fmt.Sprintf("%v %v", prefix, err))
+		}
+
+		if tmpSenderShard != senderShard {
+			panic(fmt.Sprintf("%v expect senderShard to be %v, got %v", prefix, senderShard, tmpSenderShard))
+		}
+
+		if tmpReceiverShard != receiverShard {
+			panic(fmt.Sprintf("%v expect receiverShard to be %v, got %v", prefix, receiverShard, tmpReceiverShard))
+		}
+
+		if tmpCoinType != coinType {
+			panic(fmt.Sprintf("%v expect coinType to be %v, got %v", prefix, coinType, tmpCoinType))
+		}
+
+		fmt.Printf("%v FINISHED: %v\n\n", prefix, time.Since(start).Seconds())
+	}
+}
